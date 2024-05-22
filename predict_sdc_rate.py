63ae6835a57198726c6f9fa1fa95e6002c4dac30
hjiang13 <hjiang13@kent.edu>
2024-05-22 15:27:37 -0400

    debug

--- predict_sdc_rate.py
+++ predict_sdc_rate.py
@@ -2,18 +2,53 @@ import os
 import pandas as pd
 import torch
 from transformers import RobertaTokenizer, RobertaForSequenceClassification, RobertaConfig
-
+from transformers import BertTokenizer, BertModel, AdamW
+from torch import nn
+from keybert import KeyBERT
+import torch.nn.functional as F
 # Directory containing the sliced C++ files
 input_directory = 'sliced_files'
 
 # Load CodeBERT model and tokenizer
 model_path = "../CODEBERT-REGRESSION/code/BERTRegression/checkpoint-best-acc/model.bin"
 tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
-# Create a configuration object for the model
-config = RobertaConfig.from_pretrained("microsoft/codebert-base")
-model = RobertaForSequenceClassification(config)
-model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+# Define a regression model on BERT
+class BertRegressor(nn.Module):
+    def __init__(self, bert_model="neulab/codebert-cpp", lstm_hidden_size=512, output_size=1):
+        super().__init__()
+        self.bert = BertModel.from_pretrained(bert_model)
+        self.lstm = nn.LSTM(self.bert.config.hidden_size, lstm_hidden_size, batch_first=True)
+        self.regressor = nn.Linear(lstm_hidden_size, output_size)
 
+    def forward(self, input_ids, attention_mask):
+        # Flatten input for BERT processing
+        batch_size, seq_len, chunk_size = input_ids.size()
+        input_ids = input_ids.view(-1, chunk_size)
+        attention_mask = attention_mask.view(-1, chunk_size)
+
+        with torch.no_grad():
+            bert_output = self.bert(input_ids, attention_mask=attention_mask)
+
+        # Extract [CLS] embeddings
+        cls_embeddings = bert_output.last_hidden_state[:, 0, :].view(batch_size, seq_len, -1)
+
+        # LSTM processing
+        _, (hidden, _) = self.lstm(cls_embeddings)
+        # Convert PyTorch tensor to NumPy array for KeyBERT
+        cls_embeddings_np = hidden.squeeze(0).detach().cpu().numpy()
+        print (cls_embeddings_np)
+        # Extract the key features
+        kw_model = KeyBERT()
+        keywords = kw_model.extract_keywords(cls_embeddings_np, keyphrase_ngram_range=(1, 1), stop_words='none', use_maxsum=True, nr_candidates=20, top_n=5)
+        print(keywords)
+
+        # Regression
+        return F.sigmoid(self.regressor(hidden.squeeze(0)))
+
+model = BertRegressor()
+# Evaluation
+model.load_state_dict(torch.load(model_path))
+model.eval()
 # Function to predict SDC rate using CodeBERT
 def predict_sdc_rate(source_code):
     inputs = tokenizer(source_code, return_tensors="pt", truncation=True, padding=True, max_length=512)
